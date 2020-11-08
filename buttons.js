68431e2e525ab17bf78d24b6520a4c8400c54cc3
kslash <kslasher@gmail.com>
2020-11-08 23:49:15 +0900

    :recycle: Vue.js で書き直した

--- buttons.js
+++ buttons.js
@@ -1,13 +1,29 @@
 
 (() => {
-    const buttons = document.querySelectorAll('button.sound');
+    const app = Vue.createApp({
+        data() {
+            return {
+                buttons: [{
+                    label: "hoge"
+                }]
+            }
+        },
+        beforeMount() {
+            const self = this;
+            fetch('/buttons.json')
+                .then(response => response.json())
+                .then(data => self.buttons = data);
+        },
+        methods: {
+            play(button) {
+                const file = button.label;
+                new Audio('/sounds/' + file + '.mp3').play();
+                event.preventDefault();
+                return false;
+            }
+        }
 
-    for (let button of buttons) {
-        button.addEventListener('click', (event) => {
-            const file = button.getAttribute('data-file');
-            new Audio('/sounds/' + file + '.mp3').play();
-            event.preventDefault();
-            return false;
-        });
-    }
+
+    });
+    app.mount('.buttons');
 })();
