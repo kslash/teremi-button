7d096c08ce61487b7a3aa52ee349dfb3994dfcb0
kslash <kslasher@gmail.com>
2020-12-18 04:11:45 +0900

    :sparkles: 再生モードを追加

--- buttons.js
+++ buttons.js
@@ -1,25 +1,96 @@
 
 (() => {
+
+    class Playlist {
+        constructor() {
+            this.queue = Promise.resolve();
+            this.mode = 'pause';
+        }
+
+        get mode() { return this._mode; }
+
+        set mode(mode) {
+            this._mode = mode;
+            switch (mode) {
+                case 'later':
+                    this.play = this.playLater;
+                    break;
+                case 'immediately':
+                    this.play = this.playImmediately;
+                    break;
+                case 'pause':
+                default:
+                    this.play = this.playWithPause;
+                    break;
+            }
+            this.queue = Promise.resolve();
+        }
+
+        playWithPause(audio) {
+            this.queue = this.queue.then((last) => {
+                if (last) {
+                    console.log('%s, paused:%s, ended:%s', last.src, last.paused, last.ended);
+                }
+
+                if (!last || last.ended || last.paused) {
+                    audio.play();
+                    return Promise.resolve(audio);
+                }
+
+                const p = new Promise((resolve) => {
+                    last.addEventListener('pause', (event) => {
+                        audio.play();
+                        resolve(audio);
+                    }, { once: true })
+                });
+                last.pause();
+                return p;
+            });
+        }
+        playLater(audio) {
+            this.queue = this.queue.then((last) => {
+                if (!last || last.ended) {
+                    audio.play();
+                    return Promise.resolve(audio);
+                }
+
+                return new Promise((resolve) => {
+                    last.addEventListener('ended', (event) => {
+                        audio.play();
+                        resolve(audio);
+                    }, { once: true })
+                });
+            });
+        }
+        playImmediately(audio) {
+            this.queue = this.queue.then((last) => {
+                audio.play();
+                return Promise.resolve(audio);
+            });
+        }
+
+    }
+
     const app = Vue.createApp({
         data() {
             return {
-                buttons: []
+                buttons: [],
+                playlist: new Playlist(),
             }
         },
         beforeMount() {
-            const self = this;
             fetch('/buttons.json')
                 .then(response => response.json())
-                .then(data => self.buttons = data);
+                .then(data => this.buttons = data);
         },
         methods: {
             play(event, button) {
                 const file = button.label;
-                new Audio('/sounds/' + file + '.mp3').play();
+                this.playlist.play(new Audio(`/sounds/${file}.mp3`));
                 event.preventDefault();
                 return false;
             }
         }
     });
-    app.mount('.buttons');
+    app.mount('#app');
 })();
